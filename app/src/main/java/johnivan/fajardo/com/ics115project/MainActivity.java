15627c77773af27b867b3ebbf3f86e74d6d053f0
ivan9987fajardo <ivan9987fajardo@users.noreply.github.com>
2018-11-03 14:33:11 +0800

    Major Updates

--- app/src/main/java/johnivan/fajardo/com/ics115project/MainActivity.java
+++ app/src/main/java/johnivan/fajardo/com/ics115project/MainActivity.java
@@ -1,13 +1,30 @@
 package johnivan.fajardo.com.ics115project;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void registerBtn(android.view.View v){
+        Intent i = new Intent(this,Register.class);
+        startActivity(i);
+
+    }
+
+
+
 }
